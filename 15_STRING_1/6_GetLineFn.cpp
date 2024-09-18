ed1390d1b96c5e6d943f9d37fb83b57d4af16805
Sidharth Kumar <kumarsid556@gmail.com>
2024-09-19 00:37:18 +0530

    Add files via upload

---
+++ 15_STRING_1/6_GetLineFn.cpp
@@ -0,0 +1,11 @@
+// getline is used to take input string with spaces
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"Enter String : ";
+    getline(cin,s);
+    cout<<s;
+}
