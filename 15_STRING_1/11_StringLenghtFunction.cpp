bfb7bf394831a54b56e5893342eb3013b7004cc1
Sidharth Kumar <kumarsid556@gmail.com>
2024-10-03 01:21:53 +0530

    Add files via upload

---
+++ 15_STRING_1/11_StringLenghtFunction.cpp
@@ -0,0 +1,12 @@
+// <string_name>.size(); --> used to find length or size of String
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string str = "SIDHARTH KUMAR";
+    cout<<str.length();
+    cout<<endl;
+    int lenght = str.size();
+    cout<<lenght;
+}
