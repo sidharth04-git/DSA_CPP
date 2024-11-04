3fe1c919c1435f4e1290f1dc25a1baa23f5741ea
Sidharth Kumar <kumarsid556@gmail.com>
2024-11-04 23:07:35 +0530

    Add files via upload

--- 11_ARRAY_3/30_Find_Duplicate.cpp
+++ 11_ARRAY_3/30_Find_Duplicate.cpp
@@ -4,11 +4,12 @@ int findDuplicate(vector<int> &arr){
     int n = arr.size();
     int ans = 0;
     
-    // XOR All Element
+    // XOR All Element of array with ans
 
     for(int i=0; i<n; i++){
         ans^=arr[i];
     }
+    
     // XOR 1 to n-1
 
     for(int i=1; i<n; i++){
